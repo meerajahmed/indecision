18c1ffdddd378723f84dd6f382411afeda18512b
meerajahmed <meerajahmed@gmail.com>
2018-03-17 19:12:21 +0530

    es6 default export

--- src/utils.js
+++ src/utils.js
@@ -2,9 +2,21 @@ const square = (x) => x * x;
 
 const add = (a, b) => a + b;
 
+const subtract = (a, b) => a - b;
+/*
+* default export cannot come before var declaration unlike name export.
+*
+* The right way would be:
+*  1. export default subtract;
+*  2. export default (a, b) => a - b; // export expression
+*  3. export { subtract  as default }
+*
+* */
+
 
 // named exports
 export {
   square,
-  add
+  add,
+  subtract as default
 }
